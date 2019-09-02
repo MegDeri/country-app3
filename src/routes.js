eec172f797d18b68d8e79fe514630e178f52fe6d
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-09-02 22:04:17 +0300

    Change routes

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 import Navigation from './presentational/navigation.component';
 import Home from './presentational/home.component.js';
 import Contact from './presentational/contact.component.js';
+import CountryFlagContainer from './containers/flag-container.component';
 import NotFound from './presentational/not-found.component.js';
 
 
 export default (
     <Route path='/' component={Navigation}>
         <IndexRoute component={Home}/>
+        <Route path='countries' component={CountryFlagContainer}/>
         <Route path='contact' component={Contact}/>
         <Route path='*' component={NotFound}/>
     </Route>
