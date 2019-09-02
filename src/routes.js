a5441b3966c90ebc89e903fecb418f8f4808f6c9
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-09-02 23:56:13 +0300

    Changes reducers, routes

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,17 @@ import Navigation from './presentational/navigation.component';
 import Home from './presentational/home.component.js';
 import Contact from './presentational/contact.component.js';
 import CountryFlagContainer from './containers/flag-container.component';
+import CountryDetailsContainer from './containers/country-detail-container.component';
 import NotFound from './presentational/not-found.component.js';
 
 
 export default (
     <Route path='/' component={Navigation}>
         <IndexRoute component={Home}/>
-        <Route path='countries' component={CountryFlagContainer}/>
+        <Route path='countries'>
+            <IndexRoute component={CountryFlagContainer}/>
+            <Route path='country/:id' component={CountryDetailsContainer}/>
+        </Route>/>
         <Route path='contact' component={Contact}/>
         <Route path='*' component={NotFound}/>
     </Route>
